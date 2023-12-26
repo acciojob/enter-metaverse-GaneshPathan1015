fb6597c4a46046a906511cdd25fda653d3648aa2
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-26 14:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const para = document.getElementById("status");
+const btn = document.getElementById("enterBtn");
+
+
+btn.addEventListener("click",()=>{
+	            
+	let h1ele = document.createElement("h1");
+	let text = document.createTextNode("Entered Metaverse");
+	
+     h1ele.appendChild(text);
+	para.outerHTML = h1ele.outerHTML;
+	// document.body.appendChild(h1ele);
+});
